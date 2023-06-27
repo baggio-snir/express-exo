d24516f0c59830607546d8c939dae58020a39b98
baggio-snir <71394182+baggio-snir@users.noreply.github.com>
2023-06-27 16:21:55 +0200

    Un coup de main

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.set('Content-Type', 'text/html; charset=utf-8');
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.render(path.join(__dirname, '../public/index.html'));
 });
 
 module.exports = router;
